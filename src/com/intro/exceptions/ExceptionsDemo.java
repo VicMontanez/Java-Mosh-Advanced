6b20fc777a01dd153b5b954a099278a2661dba48
VicMontanez <v_montanez@hotmail.com>
2020-04-14 22:02:29 -0400

    Exceptions - used try and catch to handle check exceptions

--- src/com/intro/exceptions/ExceptionsDemo.java
+++ src/com/intro/exceptions/ExceptionsDemo.java
@@ -1,9 +1,16 @@
 package com.intro.exceptions;
 
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 
 public class ExceptionsDemo {
     public static void show() {
-        var reader = new FileReader();
+        try {
+            var reader = new FileReader("file.txt");
+            System.out.println("File opened");
+        }
+        catch (FileNotFoundException ex) {
+            System.out.println("file does not exist.");
+        }
     }
 }
