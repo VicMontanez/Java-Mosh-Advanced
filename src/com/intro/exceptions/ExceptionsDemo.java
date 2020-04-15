b5ca5dffae51f5b23d87de7693b875ac35564dde
VicMontanez <v_montanez@hotmail.com>
2020-04-14 22:45:01 -0400

    Exceptions - try with resources statment

--- src/com/intro/exceptions/ExceptionsDemo.java
+++ src/com/intro/exceptions/ExceptionsDemo.java
@@ -2,24 +2,21 @@ package com.intro.exceptions;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        FileReader reader = null;
-
-        try {
-            reader = new FileReader("file.txt");
+        try (
+           var reader = new FileReader("file.txt");
+           var writer = new FileWriter("...");
+        ) {
             var value = reader.read();
-
         }
         catch (IOException e) {
             System.out.println("Could not read data.");
-        }
-        finally {
-            reader.close();
         }
     }
 }
