c7b201817e144eef8ad422e5a4353cdbe6f3ee81
VicMontanez <v_montanez@hotmail.com>
2020-04-16 21:52:10 -0400

    Exception - created withdraw method

--- src/com/intro/exceptions/Account.java
+++ src/com/intro/exceptions/Account.java
@@ -3,8 +3,14 @@ package com.intro.exceptions;
 import java.io.IOException;
 
 public class Account {
+    private float balance;
+
     public void deposit(float value) throws IOException{
         if (value <= 0)
             throw new IOException();
+    }
+
+    public void withdraw (float value) {
+
     }
 }
