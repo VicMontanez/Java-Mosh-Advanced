40133e2fd611ad9fba266458a9a0333ae1760bea
VicMontanez <v_montanez@hotmail.com>
2020-04-16 21:42:04 -0400

    -Exception - chanegd system out

--- src/com/intro/exceptions/ExceptionsDemo.java
+++ src/com/intro/exceptions/ExceptionsDemo.java
@@ -11,9 +11,9 @@ public class ExceptionsDemo {
     public static void show() {
         var account = new Account();
         try {
-            account.deposit(1);
+            account.deposit(-1);
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Logging");;
         }
     }
 }
