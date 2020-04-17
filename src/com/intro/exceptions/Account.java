2ccf065ddc3db1d6dccdfbc4fc4add19a6ae8f3f
VicMontanez <v_montanez@hotmail.com>
2020-04-16 21:17:39 -0400

    -Exception - created throw method

--- src/com/intro/exceptions/Account.java
+++ src/com/intro/exceptions/Account.java
@@ -2,6 +2,7 @@ package com.intro.exceptions;
 
 public class Account {
     public void deposit(float value) {
-
+        if (value <= 0)
+            throw new IllegalArgumentException();
     }
 }
