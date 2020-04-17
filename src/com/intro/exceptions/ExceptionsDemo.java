0764a53ed20468b545835583f0e97abab8a4dfc4
VicMontanez <v_montanez@hotmail.com>
2020-04-16 21:19:27 -0400

    -Exception - created account variable that throws exception

--- src/com/intro/exceptions/ExceptionsDemo.java
+++ src/com/intro/exceptions/ExceptionsDemo.java
@@ -9,14 +9,7 @@ import java.text.SimpleDateFormat;
 
 public class ExceptionsDemo {
     public static void show() {
-        try (
-           var reader = new FileReader("file.txt");
-           var writer = new FileWriter("...");
-        ) {
-            var value = reader.read();
-        }
-        catch (IOException e) {
-            System.out.println("Could not read data.");
-        }
+        var account = new Account();
+        account.deposit(-1);
     }
 }
