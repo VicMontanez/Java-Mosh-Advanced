0ed3d83a7c083b72ac54baaa38d87039faa3e565
VicMontanez <v_montanez@hotmail.com>
2020-04-18 18:35:37 -0400

    createdd number variable to use get method

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -6,6 +6,8 @@ import com.intro.generics.GenericList;
 public class Main {
     public static void main(String[] args) {
         var list = new GenericList<Integer>();
+        list.add(1);
+        int number = list.get(0);
 
     }
 }
