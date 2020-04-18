cfd5467972b3fab9e8f3f9063b3c46ed27cb103a
VicMontanez <v_montanez@hotmail.com>
2020-04-18 18:30:04 -0400

    Generics - created variable list

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -1,16 +1,11 @@
 package com.intro;
 
-import com.intro.exceptions.ExceptionsDemo;
 
-import java.io.IOException;
+import com.intro.generics.GenericList;
 
 public class Main {
-
     public static void main(String[] args) {
-        try {
-            ExceptionsDemo.show();
-        } catch (Throwable e) {
-            System.out.println("An unexpected error occurred");
-        }
+        var list = new GenericList<Integer>();
+
     }
 }
