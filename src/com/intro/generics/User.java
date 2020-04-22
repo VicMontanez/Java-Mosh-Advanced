eaab71a2cc34989283036b958e00e33819721972
VicMontanez <v_montanez@hotmail.com>
2020-04-21 20:24:33 -0400

    Generics - override tostring method

--- src/com/intro/generics/User.java
+++ src/com/intro/generics/User.java
@@ -6,4 +6,14 @@ public class User {
     public User(int points) {
         this.points = points;
     }
+
+//    @Override
+//    public int compareTo(User other) {
+//        return points - other.points;
+//    }
+
+    @Override
+    public String toString() {
+        return "Points=" + points;
+    }
 }
