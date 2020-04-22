f94c6d3edaa0dfadf031049007ea3ceac3c98995
VicMontanez <v_montanez@hotmail.com>
2020-04-21 20:28:56 -0400

    implemented methods of interface

--- src/com/intro/generics/User.java
+++ src/com/intro/generics/User.java
@@ -1,19 +1,21 @@
 package com.intro.generics;
 
-public class User {
+public class User implements Comparable<User> {
     private int points;
 
     public User(int points) {
         this.points = points;
     }
 
-//    @Override
-//    public int compareTo(User other) {
-//        return points - other.points;
-//    }
+    @Override
+    public int compareTo(User user) {
+        return 0;
+    }
 
     @Override
     public String toString() {
         return "Points=" + points;
     }
+
+
 }
