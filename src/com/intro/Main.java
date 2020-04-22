3f0cda749b395e9b9f7d1c5cebafbf0430c4be8b
VicMontanez <v_montanez@hotmail.com>
2020-04-21 20:17:29 -0400

    Generics - imported util class and used max mthod stored in variable max

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -3,12 +3,12 @@ package com.intro;
 
 import com.intro.generics.GenericList;
 import com.intro.generics.User;
+import com.intro.generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        var list = new GenericList<User>();
-        list.add(new User());
-        User user = list.get(0);
+        var max = Utils.max(1, 3);
+        System.out.println(max);
 
     }
 }
