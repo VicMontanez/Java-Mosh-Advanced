ffc5d55e02611e7e456e02d416f71be5e8f2fac8
VicMontanez <v_montanez@hotmail.com>
2020-04-24 18:41:07 -0400

    Generics - used method from utils class

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -1,14 +1,16 @@
 package com.intro;
 
 
-import com.intro.generics.GenericList;
+
+import com.intro.generics.Instructor;
 import com.intro.generics.User;
 import com.intro.generics.Utils;
 
+
 public class Main {
     public static void main(String[] args) {
-        var max = Utils.max(1, 3);
-        System.out.println(max);
+        User user = new Instructor(10);
+        Utils.printUser(new Instructor(10));
 
     }
 }
