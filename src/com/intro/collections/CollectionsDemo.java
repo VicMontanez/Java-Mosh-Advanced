193273b795c48e2deb662ba2260f17cebc06504f
VicMontanez <v_montanez@hotmail.com>
2020-04-28 00:35:27 -0400

    made new array list

--- src/com/intro/collections/CollectionsDemo.java
+++ src/com/intro/collections/CollectionsDemo.java
@@ -8,8 +8,12 @@ public class CollectionsDemo {
     public static void show () {
         Collection<String> collection = new ArrayList<> ();
         Collections.addAll(collection, "a", "b", "c");
-        Object [] objectArray = collection.toArray();
-        String [] stringArray = collection.toArray(new String[3]);
+
+        Collection<String> other = new ArrayList<>();
+        other.addAll(collection);
+
+        System.out.println(collection == other);
+        System.out.println(collection.equals(other));
         }
     }
 
