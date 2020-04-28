3d2f4a9022eb91ce91230a03ed73c063dd117a63
VicMontanez <v_montanez@hotmail.com>
2020-04-27 23:22:30 -0400

    imported and used collections class to add mutiple elements

--- src/com/intro/collections/CollectionsDemo.java
+++ src/com/intro/collections/CollectionsDemo.java
@@ -2,11 +2,13 @@ package com.intro.collections;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 
 public class CollectionsDemo {
     public static void show () {
         Collection<String> collection = new ArrayList<> ();
-
+        Collections.addAll(collection, "a", "b", "c");
+        System.out.println(collection);
         }
     }
 
