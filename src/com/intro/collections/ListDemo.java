a51168a06dc960e658d0e52babfb02aafab0a01b
VicMontanez <v_montanez@hotmail.com>
2020-04-28 23:47:03 -0400

    Collections - created array list

---
+++ src/com/intro/collections/ListDemo.java
@@ -0,0 +1,12 @@
+package com.intro.collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListDemo {
+    public static void show() {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("a");
+    }
+}
