0c4a5c00018f7ddb774bb30561d45324113ecddf
VicMontanez <v_montanez@hotmail.com>
2020-04-28 22:41:22 -0400

    created while loop

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -12,6 +12,10 @@ public class Main {
     public static void main(String[] args) {
         var list = new GenericList<String>();
         var iterator = list.iterator();
+        while (iterator.hasNext()) {
+            var current = iterator.next();
+            System.out.println(current);
+        }
 
     }
 }
