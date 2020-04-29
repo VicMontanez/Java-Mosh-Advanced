c017ed0a03b9ac3f249fd04494c1bfe9073a417f
VicMontanez <v_montanez@hotmail.com>
2020-04-28 23:48:34 -0400

    Collections - implemented list set

--- src/com/intro/collections/ListDemo.java
+++ src/com/intro/collections/ListDemo.java
@@ -1,12 +1,14 @@
 package com.intro.collections;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class ListDemo {
     public static void show() {
         List<String> list = new ArrayList<>();
-        list.add("a");
-        list.add("a");
+        Collections.addAll(list, "a", "b", "c");
+        list.set(0, "a+");
+        System.out.println(list);
     }
 }
