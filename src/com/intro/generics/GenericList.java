2288e30b8a80e82f74f5ea741dd92201882dd765
VicMontanez <v_montanez@hotmail.com>
2020-04-28 21:57:17 -0400

    Collections - changed private class to public

--- src/com/intro/generics/GenericList.java
+++ src/com/intro/generics/GenericList.java
@@ -1,7 +1,7 @@
 package com.intro.generics;
 
 public class GenericList<T extends Comparable & Cloneable> {
-    private T[] items = (T[]) new Object[10];
+    public T[] items = (T[]) new Object[10];
     private int count;
 
     public void add(T item) {
