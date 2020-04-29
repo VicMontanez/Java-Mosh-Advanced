335e8e87ee146ebdc49634d60974cf7a3dcb89f4
VicMontanez <v_montanez@hotmail.com>
2020-04-28 22:44:05 -0400

    created for each loop

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -12,10 +12,8 @@ public class Main {
     public static void main(String[] args) {
         var list = new GenericList<String>();
         var iterator = list.iterator();
-        while (iterator.hasNext()) {
-            var current = iterator.next();
-            System.out.println(current);
-        }
+        for (var item : list)
+            System.out.println(item);
 
     }
 }
