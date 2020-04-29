3cc724d45326c070e0491ac7dc26a79b8b9bb0c1
VicMontanez <v_montanez@hotmail.com>
2020-04-28 22:28:55 -0400

    Collections - declared iterator variable which hold iterator method

--- src/com/intro/Main.java
+++ src/com/intro/Main.java
@@ -2,6 +2,7 @@ package com.intro;
 
 
 
+import com.intro.generics.GenericList;
 import com.intro.generics.Instructor;
 import com.intro.generics.User;
 import com.intro.generics.Utils;
@@ -9,8 +10,8 @@ import com.intro.generics.Utils;
 
 public class Main {
     public static void main(String[] args) {
-        User user = new Instructor(10);
-        Utils.printUser(new Instructor(10));
+        var list = new GenericList<String>();
+        var iterator = list.iterator();
 
     }
 }
