0b5638b392e201bf62c73be774b69655d1098449
VicMontanez <v_montanez@hotmail.com>
2020-05-01 00:21:50 -0400

    used element method

--- src/com/intro/collections/QueueDemo.java
+++ src/com/intro/collections/QueueDemo.java
@@ -6,5 +6,11 @@ import java.util.Queue;
 public class QueueDemo {
     public static void show() {
         Queue<String> queue = new ArrayDeque<>();
+        queue.add("c");
+        queue.add("a");
+        queue.add("b");
+        // b -> a -> c
+        var front = queue.element();
+        System.out.println(front);
     }
 }
