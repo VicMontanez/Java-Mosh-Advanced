0b41e62237095773fe6508340df3656e2a8c2aa4
VicMontanez <v_montanez@hotmail.com>
2020-04-30 23:53:49 -0400

    created new ArrayDeque

--- src/com/intro/collections/QueueDemo.java
+++ src/com/intro/collections/QueueDemo.java
@@ -1,4 +1,10 @@
 package com.intro.collections;
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+
 public class QueueDemo {
+    public static void show() {
+        Queue<String> queue = new ArrayDeque<>();
+    }
 }
