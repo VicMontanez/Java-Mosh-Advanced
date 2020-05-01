8268c0b0e413344e5472ad12826c6357ad28feeb
VicMontanez <v_montanez@hotmail.com>
2020-05-01 13:56:11 -0400

    made collection string and set it to array list

--- src/com/intro/collections/SetDemo.java
+++ src/com/intro/collections/SetDemo.java
@@ -1,10 +1,12 @@
 package com.intro.collections;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
 
 public class SetDemo {
     public static void show() {
-        Set<String> set = new HashSet<>();
+        Collection<String> collection = new ArrayList<>();
     }
 }
