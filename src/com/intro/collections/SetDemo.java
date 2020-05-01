70ca6345e289dd761f00a74e752b493b754d4093
VicMontanez <v_montanez@hotmail.com>
2020-05-01 13:54:29 -0400

    decalred variable of set of string and set it to new hash set

--- src/com/intro/collections/SetDemo.java
+++ src/com/intro/collections/SetDemo.java
@@ -1,4 +1,10 @@
 package com.intro.collections;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class SetDemo {
+    public static void show() {
+        Set<String> set = new HashSet<>();
+    }
 }
