cfb8bfffcba4fa7d60ff43691aa03a109f39e793
VicMontanez <v_montanez@hotmail.com>
2020-05-01 14:00:04 -0400

    removed duplicates by put collection in a set

--- src/com/intro/collections/SetDemo.java
+++ src/com/intro/collections/SetDemo.java
@@ -1,12 +1,12 @@
 package com.intro.collections;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class SetDemo {
     public static void show() {
         Collection<String> collection = new ArrayList<>();
+        Collections.addAll(collection, "a", "b", "c", "c");
+        Set<String> set = new HashSet<>(collection);
+        System.out.println(set);
     }
 }
