1acae7b4550e23ab74cce95d3c6a6667ff7b71f8
VicMontanez <v_montanez@hotmail.com>
2020-04-30 22:52:31 -0400

    changed return to compare emails

--- src/com/intro/collections/EmailComparator.java
+++ src/com/intro/collections/EmailComparator.java
@@ -4,7 +4,7 @@ import java.util.Comparator;
 
 public class EmailComparator implements Comparator<Customer> {
     @Override
-    public int compare(Customer customer, Customer t1) {
-        return 0;
+    public int compare(Customer o1, Customer o2) {
+        return o1.getEmail().compareTo(o2.getEmail());
     }
 }
