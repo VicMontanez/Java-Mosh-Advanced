9c28d651358b9ccd1013bd23bfcd4ada65c4670a
VicMontanez <v_montanez@hotmail.com>
2020-04-30 22:47:21 -0400

    implemented method for comparator

--- src/com/intro/collections/EmailComparator.java
+++ src/com/intro/collections/EmailComparator.java
@@ -1,4 +1,10 @@
 package com.intro.collections;
 
-public class EmailComparator {
+import java.util.Comparator;
+
+public class EmailComparator implements Comparator<Customer> {
+    @Override
+    public int compare(Customer customer, Customer t1) {
+        return 0;
+    }
 }
