518c0ab6fb8aab7c2563c15efd4c7070f8d0db5a
VicMontanez <v_montanez@hotmail.com>
2020-05-02 23:00:30 -0400

    made list of customers and set it to new arraylist

--- src/com/intro/collections/MapDemo.java
+++ src/com/intro/collections/MapDemo.java
@@ -1,4 +1,10 @@
 package com.intro.collections;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MapDemo {
+    public static void show() {
+        List<Customer> customers = new ArrayList<>();
+    }
 }
