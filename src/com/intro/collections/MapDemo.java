0bb4f659960f258cb2b8662fd9b58237e463c116
VicMontanez <v_montanez@hotmail.com>
2020-05-02 23:15:16 -0400

    created map and set it to new hashmap

--- src/com/intro/collections/MapDemo.java
+++ src/com/intro/collections/MapDemo.java
@@ -1,10 +1,13 @@
 package com.intro.collections;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MapDemo {
     public static void show() {
-        List<Customer> customers = new ArrayList<>();
+        Map<String, Customer> map = new HashMap<>();
+
     }
 }
