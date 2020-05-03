4c7d3a32702984f8be5d4eedb2d9e751cabe04ab
VicMontanez <v_montanez@hotmail.com>
2020-05-02 23:19:04 -0400

    used put method to store variables in hashmap

--- src/com/intro/collections/MapDemo.java
+++ src/com/intro/collections/MapDemo.java
@@ -7,7 +7,12 @@ import java.util.Map;
 
 public class MapDemo {
     public static void show() {
+        var c1 = new Customer("a", "e1");
+        var c2 = new Customer("b", "e2");
+
         Map<String, Customer> map = new HashMap<>();
+        map.put(c1.getEmail(), c1);
+        map.put(c2.getEmail(), c2);
 
     }
 }
