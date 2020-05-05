0bc147540b9fdd4404b220768a937e0de3b4255c
VicMontanez <v_montanez@hotmail.com>
2020-05-04 23:34:31 -0400

    added static method that calls printer

--- src/com/intro/lambdas/LambdasDemo.java
+++ src/com/intro/lambdas/LambdasDemo.java
@@ -1,4 +1,11 @@
 package com.intro.lambdas;
 
 public class LambdasDemo {
+    public static void show() {
+
+    }
+
+    public static void greet(Printer printer) {
+        printer.print("Hello World");
+    }
 }
