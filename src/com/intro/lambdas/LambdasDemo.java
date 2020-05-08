cde7eaa03a18ed28451b6109ff396a836e380542
VicMontanez <v_montanez@hotmail.com>
2020-05-07 20:50:39 -0400

    declarative programming

--- src/com/intro/lambdas/LambdasDemo.java
+++ src/com/intro/lambdas/LambdasDemo.java
@@ -6,10 +6,11 @@ public class LambdasDemo {
     public static void show() {
         List<Integer> list = List.of(1, 2, 3);
 
-        //Imperative Programming
+        //Imperative Programming (for, if/else, switch/case)
         for (var item : list)
             System.out.println(item);
 
+        // Declarative Programming
         list.forEach(item -> System.out.println(item));
     }
 }
