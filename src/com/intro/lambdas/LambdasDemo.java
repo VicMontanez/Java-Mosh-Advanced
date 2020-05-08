ee61f4e22c868a870f2819268220193513faa1dc
VicMontanez <v_montanez@hotmail.com>
2020-05-07 20:44:28 -0400

    interate thru list

--- src/com/intro/lambdas/LambdasDemo.java
+++ src/com/intro/lambdas/LambdasDemo.java
@@ -5,5 +5,8 @@ import java.util.List;
 public class LambdasDemo {
     public static void show() {
         List<Integer> list = List.of(1, 2, 3);
+
+        for (var item : list)
+            System.out.println(item);
     }
 }
