0d771fc86a7774fc2ebf7e67674577957f9a2e80
VicMontanez <v_montanez@hotmail.com>
2020-05-07 20:15:04 -0400

    made lambdas expression for show method

--- src/com/intro/lambdas/LambdasDemo.java
+++ src/com/intro/lambdas/LambdasDemo.java
@@ -1,13 +1,15 @@
 package com.intro.lambdas;
 
 public class LambdasDemo {
+    public static  void print(String message) {}
     //public String prefix = "-";
     public void show() {
-        greet(message ->
-            System.out.println(message));
-
-        // Class/Object::method
-        greet(System.out::println);
+        greet(message -> print(message));
+//        greet(message ->
+//            System.out.println(message));
+//
+//        // Class/Object::method
+//        greet(System.out::println);
 
         //Printer printer = message -> System.out.println(message);
 
