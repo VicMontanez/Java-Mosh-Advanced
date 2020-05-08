e20a3c2dafd719e349690bf131549f77b3c64e26
VicMontanez <v_montanez@hotmail.com>
2020-05-07 20:49:21 -0400

    used foreach to iterate thru list

--- src/com/intro/lambdas/LambdasDemo.java
+++ src/com/intro/lambdas/LambdasDemo.java
@@ -6,7 +6,10 @@ public class LambdasDemo {
     public static void show() {
         List<Integer> list = List.of(1, 2, 3);
 
+        //Imperative Programming
         for (var item : list)
             System.out.println(item);
+
+        list.forEach(item -> System.out.println(item));
     }
 }
