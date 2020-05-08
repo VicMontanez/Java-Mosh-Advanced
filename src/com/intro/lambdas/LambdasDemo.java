3a5930b5aa1eeefc02c3b30371d5be615d6eba77
VicMontanez <v_montanez@hotmail.com>
2020-05-07 20:42:25 -0400

    created list

--- src/com/intro/lambdas/LambdasDemo.java
+++ src/com/intro/lambdas/LambdasDemo.java
@@ -1,15 +1,9 @@
 package com.intro.lambdas;
 
+import java.util.List;
+
 public class LambdasDemo {
-    public LambdasDemo(String message) {
-    }
-
     public static void show() {
-        greet(LambdasDemo::new);
-
-    }
-
-    public static void greet(Printer printer) {
-        printer.print("Hello World");
+        List<Integer> list = List.of(1, 2, 3);
     }
 }
